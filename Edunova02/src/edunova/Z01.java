46de3f2794088a19b8729f0b59946027e7394732
antoniocukrov <antonio.cukrov1@gmail.com>
2021-12-21 11:21:43 +0100

    Upload materijala sa nastave
    + z01 gotov

---
+++ Edunova02/src/edunova/Z01.java
@@ -0,0 +1,22 @@
+package edunova;
+
+import javax.swing.JOptionPane;
+
+public class Z01 {
+	
+	// Program unosi dva cijela broja
+	// Ako je njihov zbroj paran broj ispisuje Osijek
+	// inače ispisuje Zagreb
+
+	
+	public static void main(String[] args) {
+		int a = Integer.parseInt(
+				JOptionPane.showInputDialog("Unesi a")
+				);
+		int b = Integer.parseInt(
+				JOptionPane.showInputDialog("Unesi a")
+				);
+		
+		System.out.println(((a+b)%2)==0 ? "Osijek" : "Zagreb");
+	}
+}

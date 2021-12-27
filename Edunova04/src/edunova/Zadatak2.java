5c87d5b973d789aa57bc250874308e0bc28bdefb
antoniocukrov <antonio.cukrov1@gmail.com>
2021-12-27 16:28:52 +0100

    Edunova04 Z02 riješen

--- Edunova04/src/edunova/Zadatak2.java
+++ Edunova04/src/edunova/Zadatak2.java
@@ -1,12 +1,25 @@
 package edunova;
 
+import javax.swing.JOptionPane;
+
 public class Zadatak2 {
 	
 	// Za upisani cijeli broj ispisati samo prvu znamenku
-	
 	// 15262 -> 1
 	// 874 -> 8
 	
+	public static void main(String[] args) {
+		int a = Integer.parseInt(JOptionPane.showInputDialog("Unesi a"));
+		int b = a;
+		if (a>10) {
+		do {
+			a=a/10;
+			}while (a>10);
+		System.out.println(a);
+		} else System.out.println(a);
+	
+		System.out.println(b%10);
+	} 
 	
 	// podzadatak: Ispisati broj na mjestu jedinice (zadnji broj)
 	// 15262 -> 2

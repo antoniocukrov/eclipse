aaca402f0f252fedeb095ee613537fe4431592c6
antoniocukrov <antonio.cukrov1@gmail.com>
2021-12-27 16:03:46 +0100

    Edunova03 zadaci riješeni
    Nizovi i petlje

--- Edunova03/src/edunova/Zadatak3.java
+++ Edunova03/src/edunova/Zadatak3.java
@@ -4,6 +4,11 @@ public class Zadatak3 {
 	
 	// ispišite sve brojeve od 20 do 1 jedno ispod drugog
 	
-
+	public static void main(String[] args) {
+			for(int i=20;i>0;i--) {
+			System.out.println(i);
+		}
+		
+	}
 
 }

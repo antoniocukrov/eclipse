3339b58032d8bd18a08e753d8c2209e10eaae52c
antoniocukrov <antonio.cukrov1@gmail.com>
2021-12-27 20:56:57 +0100

    Ispravak uvijeta
    Pomicanje if ispred ++brojac

--- Ciklicki/src/ciklicki/Ciklicki.java
+++ Ciklicki/src/ciklicki/Ciklicki.java
@@ -13,43 +13,44 @@ public class Ciklicki {
 		int bb = b;
 		int krug = 0;
 		int brojac = 0;
-		while (brojac<=(aa*bb)) {
-		for (; b > krug; b--) {
-			cik[a - 1][b - 1] = ++brojac;
-			if (brojac==(aa*bb)) {
-				break;
+		while (brojac <= (aa * bb)) {
+			for (; b > krug; b--) {
+				if (brojac == (aa * bb)) {
+					break;
 				}
-		}
-		
-		for (b++,a--; a>krug;a--){
-			cik[a - 1][b - 1] = ++brojac;
-			if (brojac==(aa*bb)) {
-				break;
+				cik[a - 1][b - 1] = ++brojac;
+			}
+
+			for (b++, a--; a > krug; a--) {
+				if (brojac == (aa * bb)) {
+					break;
 				}
-		}
-		
-		for (a++,b++; b<=(bb-krug);++b) {
-			cik[a - 1][b - 1] = ++brojac;
-			if (brojac==(aa*bb)) {
-				break;
+				cik[a - 1][b - 1] = ++brojac;
+
+			}
+
+			for (a++, b++; b <= (bb - krug); ++b) {
+				if (brojac == (aa * bb)) {
+					break;
 				}
-		}
-		
-		for(b--,a++;a<(aa-krug);++a) {
-			cik[a - 1][b - 1] = ++brojac;
-			if (brojac==(aa*bb)) {
-				break;
+				cik[a - 1][b - 1] = ++brojac;
+
+			}
+
+			for (b--, a++; a < (aa - krug); ++a) {
+				if (brojac == (aa * bb)) {
+					break;
 				}
+				cik[a - 1][b - 1] = ++brojac;
+			}
+			krug++;
+			a--;
+			b--;
+			if (brojac == (aa * bb)) {
+				break;
+			}
 		}
-		krug++;
-		a--;
-		b--;
-		if (brojac==(aa*bb)) {
-		break;
-		}
-		}
-		
-		
+
 		for (int i = 0; i < aa; i++) {
 			System.out.println(Arrays.toString(cik[i]));
 		}

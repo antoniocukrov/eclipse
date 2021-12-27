0c3864622fb64da8a5c775b7ae8d6ac6ca0f6262
antoniocukrov <antonio.cukrov1@gmail.com>
2021-12-27 20:53:18 +0100

    Ciklicki krug

--- Ciklicki/src/ciklicki/Ciklicki.java
+++ Ciklicki/src/ciklicki/Ciklicki.java
@@ -9,18 +9,50 @@ public class Ciklicki {
 		int a = Integer.parseInt(JOptionPane.showInputDialog("Unesi A"));
 		int b = Integer.parseInt(JOptionPane.showInputDialog("Unesi B"));
 		int cik[][] = new int[a][b];
+		int aa = a;
+		int bb = b;
+		int krug = 0;
+		int brojac = 0;
+		while (brojac<=(aa*bb)) {
+		for (; b > krug; b--) {
+			cik[a - 1][b - 1] = ++brojac;
+			if (brojac==(aa*bb)) {
+				break;
+				}
+		}
 		
-		//cik[a-1][b-1] = 1;
+		for (b++,a--; a>krug;a--){
+			cik[a - 1][b - 1] = ++brojac;
+			if (brojac==(aa*bb)) {
+				break;
+				}
+		}
 		
+		for (a++,b++; b<=(bb-krug);++b) {
+			cik[a - 1][b - 1] = ++brojac;
+			if (brojac==(aa*bb)) {
+				break;
+				}
+		}
 		
-	
-		for (int i=0;i<a;i++) {
-		System.out.println(Arrays.toString(cik[i]));
+		for(b--,a++;a<(aa-krug);++a) {
+			cik[a - 1][b - 1] = ++brojac;
+			if (brojac==(aa*bb)) {
+				break;
+				}
+		}
+		krug++;
+		a--;
+		b--;
+		if (brojac==(aa*bb)) {
+		break;
+		}
 		}
 		
 		
-		
-		
-		
+		for (int i = 0; i < aa; i++) {
+			System.out.println(Arrays.toString(cik[i]));
+		}
+
 	}
 }

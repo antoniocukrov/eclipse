69b3f7f1d50d0dd5196cb252c9e77388cc2ceeed
antoniocukrov <antonio.cukrov1@gmail.com>
2021-12-28 13:35:02 +0100

    Zadatak 5 odrađen

--- Edunova04/src/edunova/Zadatak5.java
+++ Edunova04/src/edunova/Zadatak5.java
@@ -1,9 +1,21 @@
 package edunova;
 
+import javax.swing.JOptionPane;
+
 public class Zadatak5 {
-	
+
 	// Program učitava 5 cjelih brojeva
 	// foreach petljom ispisuje sve učitane brojeve
 	// ispisuje prosjek učitanih brojeva
+
+	public static void main(String[] args) {
+		int suma = 0;
+		for (int n = 1; n < 6; n++) {
+			int a = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj"));
+			System.out.println("Broj " + n + ":  " +a);
+			suma+=a;
+		}
+		System.out.println("Prosjek učitanih brojeva je: " + (float)suma/5);
+	}
 
 }

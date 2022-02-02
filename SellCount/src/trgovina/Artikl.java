d30d77b500355890cf56dc25d598cfbfc85567c1
antoniocukrov <antonio.cukrov1@gmail.com>
2022-02-02 17:26:34 +0100

    JM bye bye
    :black_joker:

--- SellCount/src/trgovina/Artikl.java
+++ SellCount/src/trgovina/Artikl.java
@@ -1,12 +1,64 @@
 package trgovina;
 
-public class Artikl extends JM extends Klasifikacija {
-	
+import svojstvaArtikla.Klasifikacija;
+
+public class Artikl extends Klasifikacija {
+
 	private Integer sifra;
 	private String naziv;
 	private String EANcode;
 	private Double cijena;
+	private String JM;
+
+	public Artikl(Integer sifra, String naziv, Integer sifra2, String naziv2, String eANcode, Double cijena,
+			String jM) {
+		super(sifra, naziv);
+		sifra = sifra2;
+		naziv = naziv2;
+		EANcode = eANcode;
+		this.cijena = cijena;
+		JM = jM;
+	}
+
 	
-	
+	public Integer getSifra() {
+		return sifra;
+	}
+
+	public void setSifra(Integer sifra) {
+		this.sifra = sifra;
+	}
+
+	public String getNaziv() {
+		return naziv;
+	}
+
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+
+	public String getEANcode() {
+		return EANcode;
+	}
+
+	public void setEANcode(String eANcode) {
+		EANcode = eANcode;
+	}
+
+	public Double getCijena() {
+		return cijena;
+	}
+
+	public void setCijena(Double cijena) {
+		this.cijena = cijena;
+	}
+
+	public String getJM() {
+		return JM;
+	}
+
+	public void setJM(String jM) {
+		JM = jM;
+	}
 
 }

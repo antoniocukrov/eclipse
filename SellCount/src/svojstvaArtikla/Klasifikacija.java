0b311ed2d9fced52ab846429d109595af91893b8
antoniocukrov <antonio.cukrov1@gmail.com>
2022-02-02 17:07:32 +0100

    Get and set

--- SellCount/src/svojstvaArtikla/Klasifikacija.java
+++ SellCount/src/svojstvaArtikla/Klasifikacija.java
@@ -4,5 +4,18 @@ public class Klasifikacija {
 	
 	private Integer sifra;
 	private String naziv;
+	
+	public Integer getSifra() {
+		return sifra;
+	}
+	public void setSifra(Integer sifra) {
+		this.sifra = sifra;
+	}
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
 
 }
